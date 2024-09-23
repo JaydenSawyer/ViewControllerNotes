e36c1023b48ff637f089722b5bc8c6bca85ee38f
Jayden Sawyer <jayden.sawyer25@d155.org>
2024-09-23 08:20:41 -0500

    Initial Commit

---
+++ ViewControllerNotesTests/ViewControllerNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerNotesTests.swift
+//  ViewControllerNotesTests
+//
+//  Created by JAYDEN SAWYER on 9/23/24.
+//
+
+import Testing
+@testable import ViewControllerNotes
+
+struct ViewControllerNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
